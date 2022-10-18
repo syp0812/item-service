a5e96d86c8303605bd6a6a7eb28cd7bc301c2602
syp0812 <94878158+syp0812@users.noreply.github.com>
2022-10-18 22:58:41 +0900

    상품 등록 처리 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -40,8 +37,11 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItem(Item item) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     @PostConstruct
