809db5c18842e87e6442e29e3da5ca840a892d94
syp0812 <94878158+syp0812@users.noreply.github.com>
2022-10-19 15:54:06 +0900

    RedirectAttributes 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -37,11 +38,13 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItem(Item item) {
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
 
-        itemRepository.save(item);
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
 
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
